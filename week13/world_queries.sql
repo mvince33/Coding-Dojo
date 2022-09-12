1f51ef5a71e33a7b581549ea9d2f53241c8097c0
Michael Vincent <30790433+mvince33@users.noreply.github.com>
2022-09-11 19:42:46 -0700

    Add files via upload

--- week13/world_queries.sql
+++ week13/world_queries.sql
@@ -2,4 +2,36 @@ USE WORLD;
 SHOW tables;
 
 SELECT *
-FROM cities;
+FROM countries
+LIMIT 5;
+
+SELECT name, continent
+FROM countries
+ORDER BY name DESC;
+
+SELECT DISTINCT continent
+FROM countries;
+
+SELECT name, continent
+FROM countries
+WHERE continent = 'Asia';
+
+SELECT name, continent
+FROM countries
+WHERE continent = 'Asia' OR continent = 'Europe';
+
+SELECT AVG(population)
+FROM countries;
+
+SELECT continent, MAX(population)
+FROM countries
+GROUP BY continent;
+
+SELECT continent, MAX(population) as Max_Pop
+FROM countries
+GROUP BY continent;
+
+SELECT continent, MAX(population) as Max_Pop
+FROM countries
+GROUP BY continent
+HAVING Max_Pop > 100000000;
